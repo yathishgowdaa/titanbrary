9835bcd688b431599a638c27c530dab43cce1623
Patrick Hovsepian <phovsepian@ucla.edu>
2018-03-05 22:41:29 -0800

    Changed cart table

--- Titanbrary.Database/Tables/Cart.sql
+++ Titanbrary.Database/Tables/Cart.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Cart]
 (
 	
-	[CartID]	UNIQUEIDENTIFIER NOT NULL,
-	[UserID]	UNIQUEIDENTIFIER NOT NULL,
-	[Date]		DATETIME NULL,
-	[BookList]	NVARCHAR(36)
-
+	[CartID]		UNIQUEIDENTIFIER NOT NULL, 
+	[UserID]		UNIQUEIDENTIFIER NOT NULL,
+	[Date]			INT NOT NULL,
+	[Quantity]		INT NOT NULL,
+	[Type]			INT NOT NULL, /* This will be used to indicate 1 == checkout , 2 == renew, 3 == return */ 
+	[Waitlist]		BIT NOT NULL, /* This will be used to indicate if the user wants to be put on a waitlist */ 
+	
     CONSTRAINT [PK_MainCartTable] PRIMARY KEY ([CartID])
 )
